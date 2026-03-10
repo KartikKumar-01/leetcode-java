51463aed6a3f82572152dcc1716d7cb36d66bf35
Kartik Kumar <108817163+KartikKumar-01@users.noreply.github.com>
2026-03-10 23:32:39 +0530

    Time: 170 ms (72.37%) | Memory: 50.1 MB (19.74%) - LeetSync

--- 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.java
+++ 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.java
@@ -3,7 +3,25 @@ class Solution {
     final int MOD = 1000000007;
     public int numberOfStableArrays(int zero, int one, int limit) {
         dp = new Integer[zero + 1][one + 1][2];
-        return (solve(zero, one, 1, limit) + solve(zero, one, 0, limit)) % MOD;
+        dp[0][0][1] = 1;
+        dp[0][0][0] = 1;
+
+        for(int oneLeft = 0; oneLeft <= one; oneLeft++){
+            for(int zeroLeft = 0; zeroLeft <= zero; zeroLeft++){
+                if(zeroLeft == 0 && oneLeft == 0) continue;
+                int res = 0;
+                for(int len = 1; len <= Math.min(limit, zeroLeft); len++){
+                    res = (res + dp[zeroLeft - len][oneLeft][0]) % MOD;
+                }
+                dp[zeroLeft][oneLeft][1] = res;
+                res = 0;
+                for(int len = 1; len <= Math.min(limit, oneLeft); len++){
+                    res = (res + dp[zeroLeft][oneLeft - len][1]) % MOD;
+                }
+                dp[zeroLeft][oneLeft][0] = res;
+            }
+        }
+        return (dp[zero][one][1] + dp[zero][one][0]) % MOD;
     }
     private int solve(int zeroLeft, int oneLeft, int last, int limit){
         if(zeroLeft == 0 && oneLeft == 0) return 1;
