b4d8cb8266709b6d77c93edc21c21edb602d6135
Kartik Kumar <108817163+KartikKumar-01@users.noreply.github.com>
2026-03-10 23:21:01 +0530

    Time: 270 ms (25.00%) | Memory: 49.4 MB (21.05%) - LeetSync

--- 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.java
+++ 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.java
@@ -1,36 +1,26 @@
 class Solution {
-
+    Integer[][][] dp ;
+    final int MOD = 1000000007;
     public int numberOfStableArrays(int zero, int one, int limit) {
-        final long MOD = 1000000007;
-        long[][][] dp = new long[zero + 1][one + 1][2];
-        for (int i = 0; i <= Math.min(zero, limit); i++) {
-            dp[i][0][0] = 1;
-        }
-        for (int j = 0; j <= Math.min(one, limit); j++) {
-            dp[0][j][1] = 1;
-        }
-        for (int i = 1; i <= zero; i++) {
-            for (int j = 1; j <= one; j++) {
-                if (i > limit) {
-                    dp[i][j][0] =
-                        dp[i - 1][j][0] +
-                        dp[i - 1][j][1] -
-                        dp[i - limit - 1][j][1];
-                } else {
-                    dp[i][j][0] = dp[i - 1][j][0] + dp[i - 1][j][1];
-                }
-                dp[i][j][0] = ((dp[i][j][0] % MOD) + MOD) % MOD;
-                if (j > limit) {
-                    dp[i][j][1] =
-                        dp[i][j - 1][1] +
-                        dp[i][j - 1][0] -
-                        dp[i][j - limit - 1][0];
-                } else {
-                    dp[i][j][1] = dp[i][j - 1][1] + dp[i][j - 1][0];
-                }
-                dp[i][j][1] = ((dp[i][j][1] % MOD) + MOD) % MOD;
+        dp = new Integer[zero + 1][one + 1][2];
+        return (solve(zero, one, 1, limit) + solve(zero, one, 0, limit)) % MOD;
+    }
+    private int solve(int zeroLeft, int oneLeft, int last, int limit){
+        if(zeroLeft == 0 && oneLeft == 0) return 1;
+        int res = 0;
+
+        if(dp[zeroLeft][oneLeft][last] != null) return dp[zeroLeft][oneLeft][last];
+
+        if(last == 1){
+            for(int i = 1; i <= Math.min(zeroLeft, limit); i++){
+                res = (res + solve(zeroLeft - i, oneLeft, 0, limit)) % MOD;
             }
         }
-        return (int) ((dp[zero][one][0] + dp[zero][one][1]) % MOD);
+        else{
+            for(int i = 1; i <= Math.min(oneLeft, limit); i++){
+                res = (res + solve(zeroLeft, oneLeft - i, 1, limit)) % MOD;
+            }
+        }
+        return dp[zeroLeft][oneLeft][last] = res % MOD;
     }
 }
