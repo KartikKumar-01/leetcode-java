20be812530017342682068dafe00aed4f9bb5e03
Kartik Kumar <108817163+KartikKumar-01@users.noreply.github.com>
2026-03-13 23:17:27 +0530

    Time: 82 ms (67.72%) | Memory: 48.5 MB (22.83%) - LeetSync

--- 1871-palindrome-partitioning-iv/palindrome-partitioning-iv.java
+++ 1871-palindrome-partitioning-iv/palindrome-partitioning-iv.java
@@ -1,40 +1,24 @@
 class Solution {
-    int[][] change;
-    int[][] dp;
     public boolean checkPartitioning(String s) {
         int n = s.length();
-        dp = new int[n][4];
-
-        for(int[] d : dp) Arrays.fill(d, -1);
-
-        change = new int[n][n];
+        boolean[][] pallin = new boolean[n][n];
+        for(int i = 0; i < n; i++)
+            pallin[i][i] = true;
         for(int len = 2; len <= n; len++){
             for(int i = 0; i + len - 1 < n; i++){
                 int j = i + len - 1;
                 if(s.charAt(i) == s.charAt(j)){
-                    change[i][j] = (len == 2) ? 0 : change[i + 1][j - 1];
-                }else{
-                    change[i][j] = (len == 2) ? 1 : change[i + 1][j - 1] + 1;
+                    pallin[i][j] = (len == 2) || pallin[i + 1][j - 1];
                 }
             }
         }
-        return solve(s, 3, 0);
-    }
-    private boolean solve(String s, int k, int i){
-        if(k == 0 && i == s.length()) return true;
-        if(k == 0 || i == s.length()) return false;
 
-        if(dp[i][k] != -1) return dp[i][k] == 1;
-        for(int j = i; j < s.length(); j++){
-            int cost = change[i][j];
-            if(cost == 0){
-                if(solve(s, k - 1, j + 1)) {
-                    dp[i][k] = 1;
-                    return true;
-                }
+        for(int i = 0; i < n - 2; i++){
+            if(!pallin[0][i]) continue;
+            for(int j = i + 1; j < n - 1; j++){
+                if(pallin[i + 1][j] && pallin[j + 1][n - 1]) return true;
             }
         }
-        dp[i][k] = 0;
         return false;
     }
 }
