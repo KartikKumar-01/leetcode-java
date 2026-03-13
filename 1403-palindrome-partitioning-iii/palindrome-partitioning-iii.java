b9ba73c5b43c5a50fac4ec437e9b16a97bfea693
Kartik Kumar <108817163+KartikKumar-01@users.noreply.github.com>
2026-03-13 23:03:03 +0530

    Time: 4 ms (86.02%) | Memory: 42.8 MB (74.15%) - LeetSync

--- 1403-palindrome-partitioning-iii/palindrome-partitioning-iii.java
+++ 1403-palindrome-partitioning-iii/palindrome-partitioning-iii.java
@@ -1,8 +1,20 @@
 class Solution {
     int[][] dp;
+    int[][] change;
     public int palindromePartition(String s, int k) {
         int n = s.length();
         dp = new int[n + 1][k + 1];
+        change = new int[n][n];
+        for(int len = 2; len <= n; len++){
+            for(int i = 0; i + len - 1 < n; i++){
+                int j = i + len - 1;
+                if(s.charAt(i) == s.charAt(j)){
+                    change[i][j] = (len == 2) ? 0 : change[i + 1][j - 1];
+                }else{
+                    change[i][j] = (len == 2) ? 1 : change[i + 1][j - 1] + 1;
+                }
+            }
+        }
         for(int[] d : dp) Arrays.fill(d, -1);
         return solve(s, k, 0);
     }
@@ -12,21 +24,11 @@ class Solution {
         if(dp[i][k] != -1) return dp[i][k];
         int ans = Integer.MAX_VALUE;
         for(int j = i; j < s.length(); j++){
-            String sbt = s.substring(i, j + 1);
-            int cost = change(sbt);
+            int cost = change[i][j];
             int next = solve(s, k - 1, j + 1);
             if(next != Integer.MAX_VALUE)
                 ans = Math.min(ans, cost + next);
         }
         return dp[i][k] = ans;
-    }
-    private int change(String s){
-        int n = s.length();
-        int cnt = 0;
-        int l = 0, r = n - 1;
-        while(l < r){
-            if(s.charAt(l++) != s.charAt(r--)) cnt++;
-        }
-        return cnt;
     }
 }
