35767a181541d820716d809f8e29e5b75baf3c96
Kartik Kumar <108817163+KartikKumar-01@users.noreply.github.com>
2026-03-14 11:33:35 +0530

    Time: 5 ms (53.94%) | Memory: 45.6 MB (45.67%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,25 +1,41 @@
 class Solution {
+
     ArrayList<String> list = new ArrayList<>();
-    char[] ch = {'a', 'b', 'c'};
+    char[] ch = {'a','b','c'};
+
     public String getHappyString(int n, int k) {
-        int total = 3 * (1 << (n - 1));
+
+        int block = 1 << (n-1);
+        int total = 3 * block;
+
         if(k > total) return "";
-        
-        solve(n, '#', new StringBuilder());
-        // Collections.sort(list);
-        if(k > list.size()) return "";
-        return list.get(k - 1);
+
+        int charType = (k-1) / block;
+
+        char first = ch[charType];
+
+        k = (k-1) % block + 1;
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(first);
+
+        solve(n, first, sb);
+
+        return list.get(k-1);
     }
+
     private void solve(int n, char last, StringBuilder cur){
-        if(cur.length() == n) {
+
+        if(cur.length() == n){
             list.add(cur.toString());
             return;
         }
+
         for(char c : ch){
             if(c != last){
                 cur.append(c);
                 solve(n, c, cur);
-                cur.deleteCharAt(cur.length() - 1);
+                cur.deleteCharAt(cur.length()-1);
             }
         }
     }
