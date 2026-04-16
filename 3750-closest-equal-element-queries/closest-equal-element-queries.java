eb8bcb37713a74185f92a043ac260427f157e14d
Kartik Kumar <108817163+KartikKumar-01@users.noreply.github.com>
2026-04-17 00:25:13 +0530

    Time: 123 ms (46.46%) | Memory: 186.9 MB (59.60%) - LeetSync

--- 3750-closest-equal-element-queries/closest-equal-element-queries.java
+++ 3750-closest-equal-element-queries/closest-equal-element-queries.java
@@ -15,21 +15,31 @@ class Solution {
                 continue;
             }
             int idx = find(cur, q);
-            if(idx == 0){
-                int x = Math.min(cur.get(idx + 1) - cur.get(idx), n - (cur.get(cur.size() - 1) - cur.get(idx)));
-                list.add(x);
-            }else if(idx == cur.size() - 1){
-                int x = Math.min(cur.get(idx) - cur.get(idx - 1), n - (cur.get(idx) - cur.get(0)));
-                list.add(x);
-            }else{
-                list.add(Math.min(cur.get(idx) - cur.get(idx - 1), cur.get(idx + 1) - cur.get(idx)));
-            }
+            // if(idx == 0){
+            //     int x = Math.min(cur.get(idx + 1) - cur.get(idx), n - (cur.get(cur.size() - 1) - cur.get(idx)));
+            //     list.add(x);
+            // }else if(idx == cur.size() - 1){
+            //     int x = Math.min(cur.get(idx) - cur.get(idx - 1), n - (cur.get(idx) - cur.get(0)));
+            //     list.add(x);
+            // }else{
+            //     list.add(Math.min(cur.get(idx) - cur.get(idx - 1), cur.get(idx + 1) - cur.get(idx)));
+            // }
+            int size = cur.size();
+            int right = cur.get((idx + 1) % size);
+            int d1 = Math.abs(right - q);
+            int cd1 = n - d1;
+            int x = Math.min(d1, cd1);
+            int left = cur.get((idx - 1 + size) % size);
+            int d2 = Math.abs(left - q);
+            int cd2 = n - d2;
+            x = Math.min(x, Math.min(cd2, d2));
+            list.add(x);
         }
         return list;
     }
     private int find(List<Integer> cur, int q){
         int ans = -1;
-        int low = 0, high = cur.size();
+        int low = 0, high = cur.size() - 1;
         while(low <= high){
             int mid = low + (high - low) / 2;
             if(cur.get(mid) == q){
