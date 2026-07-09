e02c613659efa108d81cd264e324f00a8236904a
Kartik Kumar <108817163+KartikKumar-01@users.noreply.github.com>
2026-07-10 01:46:46 +0530

    Time: 15 ms (29.31%) | Memory: 113.5 MB (39.21%) - LeetSync

--- 3730-maximum-subarray-sum-after-multiplier/maximum-subarray-sum-after-multiplier.java
+++ 3730-maximum-subarray-sum-after-multiplier/maximum-subarray-sum-after-multiplier.java
@@ -5,23 +5,29 @@ class Solution {
     }
     public long solve(int[] nums, int k, int flag) {
         int n = nums.length;
-        long[] p0 = new long[n];
-        long[] p1 = new long[n];
-        long[] p2 = new long[n];
+        // long[] p0 = new long[n];
+        // long[] p1 = new long[n];
+        // long[] p2 = new long[n];
 
-        p0[0] = nums[0];
-        p1[0] = op(nums[0] * 1L, k, flag);
-        p2[0] = Long.MIN_VALUE / 2;
+        // p0[0] = nums[0];
+        // p1[0] = op(nums[0] * 1L, k, flag);
+        // p2[0] = Long.MIN_VALUE / 2;
+        long p0 = nums[0];
+        long p1 = op(nums[0] * 1L, k, flag);
+        long p2 = Long.MIN_VALUE / 2;
 
         
-        long ans = Math.max(p0[0], p1[0]);
+        long ans = Math.max(p0, p1);
         for(int i = 1; i < n; i++){
             long val = op(nums[i] * 1L, k, flag);
-            p0[i] = Math.max(p0[i - 1] + nums[i], nums[i]);
-            p1[i] = Math.max(val, Math.max(val + p0[i - 1], p1[i - 1] + val));
-            p2[i] = Math.max(p1[i - 1] + nums[i], p2[i - 1] + nums[i]);
-            long cur = Math.max(p0[i], Math.max(p1[i], p2[i]));
+            long cp0 = Math.max(p0 + nums[i], nums[i]);
+            long cp1 = Math.max(val, Math.max(val + p0, p1 + val));
+            long cp2 = Math.max(p1 + nums[i], p2 + nums[i]);
+            long cur = Math.max(cp0, Math.max(cp1, cp2));
             ans = Math.max(ans, cur);
+            p0 = cp0;
+            p1 = cp1;
+            p2 = cp2;
         }
         // for(int i = 0; i < n; i++){
         //     ans = Math.max(ans, Math.max(p0[i], Math.max(p1[i], p2[i])));
