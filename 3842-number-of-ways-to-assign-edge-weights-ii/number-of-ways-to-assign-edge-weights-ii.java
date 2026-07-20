1158c42627facbb29c54d8243566803d769a6a54
Kartik Kumar <108817163+KartikKumar-01@users.noreply.github.com>
2026-07-21 01:28:51 +0530

    Time: 147 ms (36.36%) | Memory: 294.1 MB (55.23%) - LeetSync

--- 3842-number-of-ways-to-assign-edge-weights-ii/number-of-ways-to-assign-edge-weights-ii.java
+++ 3842-number-of-ways-to-assign-edge-weights-ii/number-of-ways-to-assign-edge-weights-ii.java
@@ -1,113 +1,106 @@
 class Solution {
-    static int[] binaryExponent;
+    static int[] power;
     static final long mod = 1000000007l;
 
     static {
-        binaryExponent = new int[100001];
-        long pro = 1l;
-        for (int i = 1; i < binaryExponent.length; i++) {
-            binaryExponent[i] = (int) (pro);
-            pro = (pro * 2l) % mod;
+        power = new int[100001];
+        power[0] = 1;
+        for (int i = 1; i < power.length; i++) {
+            power[i] = (int) ((2L * power[i - 1]) % mod);
         }
     }
-    
-    int[][] ancestors;
-    int[] ans, parent;
+    int[] parent;
     int[] depth;
-    boolean[] visited;
-
-    private void buildAncestors(int[] parent, int numNodes) {
-        ancestors = new int[(int) (Math.log(numNodes) / Math.log(2)) + 1][numNodes + 1];
-        int n = ancestors.length, m = ancestors[0].length;
-        for (int i = 0; i < m; i++) {
-            ancestors[0][i] = parent[i];
-        }
-
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
-                ancestors[i][j] = ancestors[i - 1][ancestors[i - 1][j]];
-            }
-        }
-    }
-
+    boolean[] vis;
+    int[][] up;
+    int col;
     public int[] assignEdgeWeights(int[][] edges, int[][] queries) {
-        int n = edges.length, q = queries.length;
-        ans = new int[q];
+        int n = edges.length + 1;
+        int q = queries.length;
         parent = new int[n + 2];
-        visited = new boolean[n + 2];
         depth = new int[n + 2];
+        vis = new boolean[n + 2];
 
         List<List<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i <= n + 1; i++) {
-            adj.add(new ArrayList<>());
-        }
-        for (int i = 0; i < edges.length; i++) {
-            adj.get(edges[i][0]).add(edges[i][1]);
-            adj.get(edges[i][1]).add(edges[i][0]);
+        for(int i = 0; i <= n; i++) adj.add(new ArrayList<>());
+        for(int[] e : edges){
+            int u = e[0], v = e[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
         }
 
-        parent[1] = 1;
 
+        parent[1] = -1;
         dfs(1, adj, 0);
-        buildAncestors(parent, n + 1);
-        
-        for (int i = 0; i < q; i++) {
-            ans[i] = binaryExponent[getPathCount(queries[i][0], queries[i][1])];
+        createUp(n, parent);
+
+        int[] ans = new int[q];
+        for(int i = 0; i < q; i++){
+            int u = queries[i][0], v = queries[i][1];
+            int dist = depth[u] + depth[v] - 2 * depth[lca(u, v)];
+            ans[i] = dist == 0 ? 0 : power[dist - 1];
         }
         return ans;
     }
 
-    private int getPathCount(int u, int v) {
-        int depthU = depth[u], depthV = depth[v], lca = -1;
+    private int lca(int u, int v){
+        int du = depth[u];
+        int dv = depth[v];
+        if(du < dv) {
+            int temp = u;
+            u = v;
+            v = temp;
 
-        if (depthU > depthV) lca = LCA(u, v);
-        else lca = LCA(v, u);
-    
-        return depth[u] + depth[v] - 2 * depth[lca];
-    }
+            temp = du;
+            du = dv;
+            dv = temp;
+        }
 
-    private int LCA(int u, int v) {
-        int depthU = depth[u];
-        int depthV = depth[v];
+        int diff = du - dv;
+        u = getAncestor(u, diff);
+        if(u == v) return u;
 
-        int diff = depthU - depthV;
-        int mask = 1, idx = 0;
-
-        while (mask <= diff) {
-            if ((mask & diff) > 0) {
-                u = ancestors[idx][u];
+        for(int j = col - 1; j >= 0; j--){
+            if(up[u][j] == -1) continue;
+            if(up[u][j] != up[v][j]){
+                u = up[u][j];
+                v = up[v][j];
             }
-
-            mask <<= 1;
-            idx++;
         }
+        return up[u][0];
+    }
 
-        if (u == v) return u;
+    private void createUp(int n, int[] parent){
+        col = 32 - Integer.numberOfLeadingZeros(n);
+        up = new int[n + 2][col];
+        for(int[] u : up) Arrays.fill(u, -1);
+        for(int i = 1; i <= n; i++) up[i][0] = parent[i];
 
-        int maxJumps = ancestors.length - 1;
-
-        while (maxJumps >= 0) {
-            if (ancestors[maxJumps][u] != ancestors[maxJumps][v]) {
-                u = ancestors[maxJumps][u];
-                v = ancestors[maxJumps][v];
+        for(int j = 1; j < col; j++){
+            for(int i = 1; i <= n; i++){
+                if(up[i][j - 1] != -1)
+                    up[i][j] = up[up[i][j - 1]][j - 1];
             }
-            maxJumps--;
-        }
-
-        int lca = ancestors[0][u];
-        return lca;
-    }
-
-    private void dfs(int node, List<List<Integer>> adj, int depthValue) {
-        visited[node] = true;
-        depth[node] = depthValue;
-        List<Integer> children = adj.get(node);
-
-        for (int v : children) {
-            if (visited[v]) continue;
-            parent[v] = node;
-            dfs(v, adj, depthValue + 1);
         }
     }
 
+    private int getAncestor(int node, int k){
+        for(int b = 0; b < col; b++){
+            if((k & (1 << b)) != 0){
+                node = up[node][b];
+            }
+        }
+        return node;
+    }
+
+    private void dfs(int i, List<List<Integer>> adj, int d){
+        vis[i] = true;
+        depth[i] = d;
+        for(int next : adj.get(i)){
+            if(!vis[next]){
+                parent[next] = i;
+                dfs(next, adj, d + 1);
+            }
+        }
+    }
 }
