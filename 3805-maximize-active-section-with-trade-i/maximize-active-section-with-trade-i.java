da09cb74c3292c8fd54fd4357772e3bdb2f9f955
Kartik Kumar <108817163+KartikKumar-01@users.noreply.github.com>
2026-07-22 00:37:09 +0530

    Time: 92 ms (69.01%) | Memory: 47.8 MB (83.10%) - LeetSync

--- 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.java
+++ 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.java
@@ -1,10 +1,12 @@
 class Solution {
     public int maxActiveSectionsAfterTrade(String s) {
-        List<Integer> list = new ArrayList<>();
+        // List<Integer> list = new ArrayList<>();
         int ans = 0;
         for(char c : s.toCharArray())if(c == '1') ans++;
         int n = s.length();
         int i = 0;
+        int prev = 0;
+        int mx = 0;
         while(i < n){
             if(s.charAt(i) == '0'){
                 int c = 0;
@@ -12,13 +14,10 @@ class Solution {
                     c++;
                     i++;
                 }
-                list.add(c);
+                if(prev != 0) mx = Math.max(mx, prev + c);
+                prev = c;
             }
             i++;
-        }
-        int mx = 0;
-        for(int j = 0; j < list.size() - 1; j++){
-            mx = Math.max(mx, list.get(j) + list.get(j + 1));
         }
         return mx + ans;
     }
