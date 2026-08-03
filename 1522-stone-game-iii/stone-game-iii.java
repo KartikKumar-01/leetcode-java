f8c15f669733fe4bdf3155a6be6bcb7342526ad5
Kartik Kumar <108817163+KartikKumar-01@users.noreply.github.com>
2026-08-03 21:16:43 +0530

    Time: 10 ms (83.24%) | Memory: 85.4 MB (71.59%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.java
+++ 1522-stone-game-iii/stone-game-iii.java
@@ -1,34 +1,46 @@
 class Solution {
-    Integer[][] dp;
+    // Integer[][] dp;
     public String stoneGameIII(int[] stoneValue) {
         int n = stoneValue.length;
-        dp = new Integer[n][2];
-        int ans = helper(stoneValue, 0, 0);
+        // dp = new Integer[n][2];
+        int[] dp = new int[n + 1];
+ 
+
+        for(int i = n - 1; i >= 0; i--){
+            dp[i] = Integer.MIN_VALUE;
+            int sum = 0;
+            for(int j = 0; j < 3; j++){
+                if(i + j >= n) break;
+                sum += stoneValue[i + j];
+                dp[i] = Math.max(dp[i], sum - dp[i + j + 1]);
+            }
+        }
+        int ans = dp[0];
         if(ans > 0) return "Alice";
         else if(ans < 0) return "Bob";
         return "Tie";
     }
-    private int helper(int[] a, int i, int p){
-        if(i >= a.length) return 0;
-        if(dp[i][p] != null) return dp[i][p];
-        int ans = 0;
-        int sum = 0;
+    // private int helper(int[] a, int i, int p){
+    //     if(i >= a.length) return 0;
+    //     if(dp[i][p] != null) return dp[i][p];
+    //     int ans = 0;
+    //     int sum = 0;
         
-        if(p == 0){
-                ans = Integer.MIN_VALUE;
-            for(int j = 0; j < 3; j++){
-                if(i + j >= a.length) break;
-                sum += a[i + j];
-                ans = Math.max(ans, sum + helper(a, i + j + 1, 1));
-            }
-        }else{
-                ans = Integer.MAX_VALUE;
-            for(int j = 0; j < 3; j++){
-                if(i + j >= a.length) break;
-                sum -= a[i + j];
-                ans = Math.min(ans, sum + helper(a, i + j + 1, 0));
-            }
-        }
-        return dp[i][p] = ans;
-    }
+    //     if(p == 0){
+    //             ans = Integer.MIN_VALUE;
+    //         for(int j = 0; j < 3; j++){
+    //             if(i + j >= a.length) break;
+    //             sum += a[i + j];
+    //             ans = Math.max(ans, sum + helper(a, i + j + 1, 1));
+    //         }
+    //     }else{
+    //             ans = Integer.MAX_VALUE;
+    //         for(int j = 0; j < 3; j++){
+    //             if(i + j >= a.length) break;
+    //             sum -= a[i + j];
+    //             ans = Math.min(ans, sum + helper(a, i + j + 1, 0));
+    //         }
+    //     }
+    //     return dp[i][p] = ans;
+    // }
 }
