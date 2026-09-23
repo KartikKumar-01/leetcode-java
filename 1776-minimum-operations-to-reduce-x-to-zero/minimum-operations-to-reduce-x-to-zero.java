1980f4b62c86abd495530e4508f65d2a073ad2f6
Kartik Kumar <108817163+KartikKumar-01@users.noreply.github.com>
2026-09-24 01:06:04 +0530

    Time: 89 ms (5.08%) | Memory: 168.1 MB (5.01%) - LeetSync

---
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minOperations(int[] nums, int x) {
+        int n = nums.length;
+        int tar = Arrays.stream(nums).sum() - x;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int mx = 0;
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            sum += nums[i];
+            if(map.containsKey(sum - tar)){
+                int j = map.get(sum - tar);
+                mx = Math.max(mx, i - j);
+            }
+            if(!map.containsKey(sum)) map.put(sum, i);
+        }
+        return mx == 0 && tar != 0 ? -1 : n - mx;
+    }
+}
